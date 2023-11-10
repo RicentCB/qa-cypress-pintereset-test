59901b9384d1b67417f0026a6d97edc057461c52
“RicentCB“ <ricardo.cb009@gmail.com>
2023-11-09 20:15:20 -0600

    [ADD] Blue pieces assert

--- cypress/e2e/board.feature
+++ cypress/e2e/board.feature
@@ -1,7 +1,11 @@
 Feature: Checkers Board
     The user is able to use the board, and play with the orange pieces
     
-    Scenario: The user can use the Restart Button
+    # Scenario: The user can use the Restart Button
+    #     When The user navigate to the home page
+    #     Then Click on the Restart Button
+    #     Then Check if the page has been loaded correctly
+    
+    Scenario: The user can see only 12 blue pieces
         When The user navigate to the home page
-        Then Click on the Restart Button
-        Then Check the if the page has been loaded correctly
+        Then Check if there are just 12 blue pieces in the board
--- cypress/e2e/board.ts
+++ cypress/e2e/board.ts
@@ -6,6 +6,18 @@ When("The user navigate to the home page", (): void => { cy.visit(HomePage.baseU
 Then("Click on the Restart Button", (): void => {
     HomePage.getRestartButton().click();
 })
-Then("Check the if the page has been loaded correctly", (): void => {
+Then("Check if the page has been loaded correctly", (): void => {
     cy.url().should("eq", HomePage.baseUrl);
+})
+
+/* Check The blue pieces */
+Then("Check if there are just 12 blue pieces in the board", (): void => {
+    let bluePiecesCont = 0;
+    HomePage.getTheBoard().find('img').each((space: JQuery<HTMLElement>): void => {
+        if (space.attr("src") === "me1.gif") {
+            bluePiecesCont++
+        }
+    }).then((): void => {
+        cy.wrap(bluePiecesCont).should("eq", 12, "There are 12 blue pieces");
+    })
 })
