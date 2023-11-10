bd264a879ab9ebee093e3dfcfba29916fcdf9f11
“RicentCB“ <ricardo.cb009@gmail.com>
2023-11-09 21:02:10 -0600

    [ADD] Selectors

--- cypress/pageObject/homePage.ts
+++ cypress/pageObject/homePage.ts
@@ -4,13 +4,17 @@ class HomePageObject {
     baseUrl: string = BaseUrl;
     selectors: { [key: string]: string } = {
         "MENU_OPTIONS": "p.footnote",
-        "BOARD": ".boardWrapper #board"
+        "BOARD": ".boardWrapper #board",
+        "INFO_MESSAGE": ".gameWrapper #message"
     }
     getRestartButton(): Cypress.Chainable<JQuery> {
         return cy.get(this.selectors.MENU_OPTIONS).contains("Restart...");
     }
     getTheBoard(): Cypress.Chainable {
-        return cy.get(this.selectors.BOARD);
+        return cy.get(this.selectors.BOARD).find("img");
+    }
+    getInfoMeesage(): Cypress.Chainable<JQuery> {
+        return cy.get(this.selectors.INFO_MESSAGE);
     }
 }
 
