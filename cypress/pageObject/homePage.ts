7d91dd71e5df6b82b217d112b40d01a3155d1005
“RicentCB“ <ricardo.cb009@gmail.com>
2023-11-09 20:15:02 -0600

    [ADD] Board Selector

--- cypress/pageObject/homePage.ts
+++ cypress/pageObject/homePage.ts
@@ -3,10 +3,14 @@ import { BaseUrl } from '../support/const'
 class HomePageObject {
     baseUrl: string = BaseUrl;
     selectors: { [key: string]: string } = {
-        "MENU_OPTIONS": "p.footnote"
+        "MENU_OPTIONS": "p.footnote",
+        "BOARD": ".boardWrapper #board"
     }
     getRestartButton(): Cypress.Chainable<JQuery> {
         return cy.get(this.selectors.MENU_OPTIONS).contains("Restart...");
+    }
+    getTheBoard(): Cypress.Chainable {
+        return cy.get(this.selectors.BOARD);
     }
 }
 
