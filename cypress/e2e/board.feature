6933250796bd6cb3c1acdb64af93022913bda0f7
“RicentCB“ <ricardo.cb009@gmail.com>
2023-11-09 20:23:52 -0600

    [FEAT] Verify the number of pieces

--- cypress/e2e/board.feature
+++ cypress/e2e/board.feature
@@ -6,6 +6,7 @@ Feature: Checkers Board
     #     Then Click on the Restart Button
     #     Then Check if the page has been loaded correctly
     
-    Scenario: The user can see only 12 blue pieces
+    Scenario: The user can see only 12 blue and 12 orange pieces
         When The user navigate to the home page
         Then Check if there are just 12 blue pieces in the board
+        Then Check if there are just 12 orange pieces in the board
--- cypress/e2e/board.ts
+++ cypress/e2e/board.ts
@@ -10,14 +10,24 @@ Then("Check if the page has been loaded correctly", (): void => {
     cy.url().should("eq", HomePage.baseUrl);
 })
 
-/* Check The blue pieces */
-Then("Check if there are just 12 blue pieces in the board", (): void => {
-    let bluePiecesCont = 0;
+/* Check the number of pieces */
+const checkNumberPieces = (color: "orange" | "blue", number: number): void => {
+    const colorToImage: { [key: string]: string } = {
+        "blue": "me1.gif",
+        "orange": "me1.gif",
+    }
+    let piecesCont: number = 0;
     HomePage.getTheBoard().find('img').each((space: JQuery<HTMLElement>): void => {
-        if (space.attr("src") === "me1.gif") {
-            bluePiecesCont++
+        if (space.attr("src") === colorToImage[color]) {
+            piecesCont++
         }
     }).then((): void => {
-        cy.wrap(bluePiecesCont).should("eq", 12, "There are 12 blue pieces");
+        cy.wrap(piecesCont).should("eq", number, `There are ${number} ${color} pieces`);
     })
+}
+Then("Check if there are just 12 blue pieces in the board", (): void => {
+    checkNumberPieces("blue", 12);
+})
+Then("Check if there are just 12 orange pieces in the board", (): void => {
+    checkNumberPieces("orange", 12);
 })
